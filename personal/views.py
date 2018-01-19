4e051fad23703f716c3a80b4f6704247bce46ff9
Mikhail <mixandoma@ya.ru>
2018-01-19 15:51:59 +0500

    contact page created and interlinked

--- personal/views.py
+++ personal/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'personal/home.html')
+
+
+def contact(request):
+    return render(request, 'personal/basic.html', {'info': {'name': 'James',
+                                                            'lastname': 'Baxter',
+                                                            'phone': '724235124'}
+                                                   }
+                  )
