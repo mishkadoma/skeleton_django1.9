63065cec97c26fb398cc0c64ad0c93b503bb777b
Mikhail <mixandoma@ya.ru>
2018-01-20 09:25:47 +0500

    starting blog app

--- personal/views.py
+++ personal/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 def index(request):
@@ -9,6 +9,4 @@ def index(request):
 def contact(request):
     return render(request, 'personal/basic.html', {'info': {'name': 'James',
                                                             'lastname': 'Baxter',
-                                                            'phone': '724235124'}
-                                                   }
-                  )
+                                                            'phone': '724235124'})
